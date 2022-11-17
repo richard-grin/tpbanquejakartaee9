c8830d28ab19deea96cedb873401f136a9901c22
Richard Grin <grin@unice.fr>
2022-11-17 21:03:02 +0100

    Ajout EJB GestionnaireCompte.

---
+++ src/main/java/fr/grin/tpbanque/ejb/GestionnaireCompte.java
@@ -0,0 +1,44 @@
+package fr.grin.tpbanque.ejb;
+
+import fr.grin.tpbanque.entities.CompteBancaire;
+import jakarta.annotation.sql.DataSourceDefinition;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import java.util.List;
+
+/**
+ *
+ * @author grin
+ */
+@DataSourceDefinition(
+        className = "com.mysql.cj.jdbc.MysqlDataSource",
+        name = "java:app/jdbc/banque",
+        serverName = "localhost",
+        portNumber = 3306,
+        user = "toto", // nom et
+        password = "ric088", // mot de passe que vous avez donnés lors de la création de la base de données
+        databaseName = "banque",
+        properties = {
+            "useSSL=false",
+            "allowPublicKeyRetrieval=true"
+        }
+)
+@Stateless
+public class GestionnaireCompte {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public void creerCompte(CompteBancaire c) {
+        em.persist(c);
+    }
+
+    public List<CompteBancaire> getAllComptes() {
+        TypedQuery query = 
+                em.createQuery("select c from CompteBancaire c", CompteBancaire.class);
+        return query.getResultList();
+    }
+
+}
